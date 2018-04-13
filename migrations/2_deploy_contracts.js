40adbc48ead00364e89fd8aac855a30ce160c51b
Bagaric <joseph@josephit.com>
2018-04-13 17:35:38 +0700

    Added the faucet to deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const HumanStandardToken = artifacts.require("./HumanStandardToken.sol");
+const DAYFaucet = artifacts.require("./DAYFaucet.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(HumanStandardToken, 1000000, 'DAY Token', 18, 'DAY');
+	deployer.deploy(HumanStandardToken,1000000, 'DAY Token', 18, 'DAY').then(function() {
+    	return deployer.deploy(DAYFaucet, HumanStandardToken.address, 333, 60);
+	});
 };
