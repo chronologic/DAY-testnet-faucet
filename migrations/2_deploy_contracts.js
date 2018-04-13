7dd3d2089d37a4c6b9fe812bbe6fc5af48d9c819
Bagaric <joseph@josephit.com>
2018-04-13 17:44:32 +0700

    Added saving the contract addresses to a file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,19 @@
-const HumanStandardToken = artifacts.require("./HumanStandardToken.sol");
+const fs = require('fs')
+
+const DAYToken = artifacts.require("./HumanStandardToken.sol");
 const DAYFaucet = artifacts.require("./DAYFaucet.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(HumanStandardToken,1000000, 'DAY Token', 18, 'DAY').then(function() {
-    	return deployer.deploy(DAYFaucet, HumanStandardToken.address, 333, 60);
-	});
+	deployer.deploy(DAYToken, 1000000, 'DAY Token', 18, 'DAY')
+	.then(() => {
+    	return deployer.deploy(DAYFaucet, DAYToken.address, 333, 60);
+	})
+	.then(() => {
+      const contracts = {
+        DAYToken: DAYToken.address,
+        DAYFaucet: DAYFaucet.address,
+      };
+
+      fs.writeFileSync('DAY_contracts.json', JSON.stringify(contracts));
+    });
 };
